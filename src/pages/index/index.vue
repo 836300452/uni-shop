88d19a5bad1818f0331ef98537ceed78c32f2de8
‘836300452’ <836300452@qq.com>
2023-08-09 17:35:16 +0800

    初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <uni-card title="基础卡片" :isFull="true" sub-title="副标题" extra="额外信息">
+    <text>这是一个通栏卡片 ，通栏没有外边距，左右会贴合父元素。</text>
+  </uni-card>
+</template>
+
+<style lang="scss">
+//
+</style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="my">my</view>
+</template>
+
+<style lang="scss">
+//
+</style>
